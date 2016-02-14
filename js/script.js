7099b2cbccf555420a1ebe654106a82c0c756ba1
tadashi0924 <tadashi.shinohara0924@gmail.com>
2016-02-14 20:04:22 +0900

    updated

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
-function run(){
 
+function run(){
+	
 	document.getElementById("calc").onclick = function(){
+		
+		/*for(var i = 0; i < (document.keyfactors.elements.length) - 1; i++){
+			var num = document.keyfactors.elements[i].value;
+			if(num != null){
+				if(isNaN(num)){
+					alert("Please input numbers!");
+					num = num.defaultValue;
+					num.focus();
+					num.select();
+				}
+			}
+		}*/
+
+
 
 		//Assumption Input
 
@@ -20,21 +35,7 @@ function run(){
 
 		//5. Method of Depreciation => Straight-Line
 
-		/*window.onload = function(){
-			document.getElementById("calc").onclick = function(){
-				for(var i = 0; i < (document.keyfactors.elements.length) - 1; i++){
-					var num = document.keyfactors.elements[i].value;
-					if(num != null){
-						if(isNaN(num)){
-							alert("Please input numbers!");
-							num = num.defaultValue;
-							num.focus();
-							num.select();
-						}
-					}
-				}
-			};
-		};*/
+		
 		
 		//Cash Flows
 		
@@ -190,7 +191,7 @@ function run(){
 			hide(document.getElementsByClassName("comments")[0]);
 			show(document.getElementsByClassName("nogo-comments")[0]);
 		}
-	}
+	};
 
 	function fadeIn(elem, ms){
   		if(!elem){
@@ -254,6 +255,14 @@ function run(){
 		fadeOut(document.getElementById("explanation1"), 400);
 	});
 
+	document.getElementById("costOfCapital").addEventListener("click", function(){
+		fadeIn(document.getElementById("industry_return"), 400);
+	});
+
+	document.getElementById("costOfCapital").addEventListener("mouseout", function(){
+		fadeOut(document.getElementById("industry_return"), 400);
+	});
+
 	document.getElementById("cashflows").addEventListener("click", function(){
 		fadeIn(document.getElementById("explanation2"), 400);
 	});
@@ -269,6 +278,94 @@ function run(){
 	document.getElementById("calculation").addEventListener("mouseout", function(){
 		fadeOut(document.getElementById("explanation3"), 400);
 	});
+	
+
+
+
+
+
+	// 10 Industry Portfolios (Monthly)
+	// 10 years data from 2005-08-31 to 2015-07-31
+	var myurl = "https://www.quandl.com/api/v3/datasets/KFRENCH/10_IND_PORTF_M.json?api_key=zygEkvv9GeTHH7Ep5M3y&start_date=2005-08-31";
+	var request = new XMLHttpRequest();
+	request.open("GET", myurl, true);
+
+	request.onload = function(){
+		if(request.status >= 200 && request.status < 400){
+			var data = JSON.parse(request.responseText);
+			industry_names = data["dataset"]["column_names"];
+			indexArray = data["dataset"]["data"];
+
+			var industryData = [];
+
+			for(var i = 1; i < industry_names.length; i++){
+				industryData[i - 1] = {	industry: industry_names[i],
+									total: 0,
+									average: 0,
+									median: 0};
+			}
+
+			for(var row = 1; row <= 10; row++){
+				var total = 0;
+				var average = 0;
+				var medianArray = [];
+				for(var column = 0; column < indexArray.length; column++){
+					total += indexArray[column][row];
+					medianArray.push(indexArray[column][row]);
+				}
+
+				industryData[row - 1]["total"] = total;
+
+				// Average calculation
+				monthlyAvg = total / indexArray.length; // Monthly
+				annualAvg = Math.round((monthlyAvg * 12) * 100) / 100; // Annual
+				industryData[row - 1]["average"] = annualAvg;
+
+				// Median calculation (Median of 120 months; 
+				// An annual median by assuming the median of a month * 12;
+
+				industryData[row - 1]["median"] = Math.round((medianCalc(medianArray) * 12) * 100) / 100;
+			}
+			
+			
+			// If the difference betwewen average and median is big, take the median.
+	
+			function avgVsMedian(arr){
+					if(Math.abs(arr["average"] - arr["median"]) > 3){
+						return arr["median"];
+					}else{
+						return arr["average"];
+					}
+			}
+
+
+			function medianCalc(arr, fn){
+				var half = (arr.length / 2) | 0;
+				var temp = arr.sort(fn);
+
+				if(temp.length % 2){
+					return temp[half];
+				}
+				return (temp[half - 1] + temp[half]) / 2;
+			}
+
+
+			for(var i = 0; i < document.getElementsByTagName("td").length; i++){
+				var el = document.getElementsByTagName("td")[i];
+				el.innerHTML = avgVsMedian(industryData[i]);
+			}
+
+
+		}else{
+			alert("Error returned from the server");
+		}
+	}
+
+	request.onerror = function(){
+		alert("Connection Error");
+	}
+
+	request.send();
 
 }
 
@@ -284,4 +381,30 @@ if(document.readyState != "loading"){
 		}
 	});
 }
+
+/*function hasClass(el, className){
+	return el.classList ? el.classList.contains(className) : 
+		new RegExp("\\b" + className + "\\b").test(el.className);
+}
+
+function addClass(el, className){
+	if(el.classList){
+		el.classList.add(className);
+	}else if(!hasClass(el, className)){
+		el.className += " " + className;
+	}
+}
+
+function removeClass(el, className){
+	if(el.classList){
+		el.classList.remove(className);
+	}else{
+		el.className = el.className.replace(new RegExp("\\b" + className + "\\b", "g"), "");
+	}
+}
+
+function toggle(el){
+		hasClass(el, "is_hidden") ? removeClass(el, "is_hidden") : addClass(el, "is_hidden");
+}*/
+
 
